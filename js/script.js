231f12b4fb306c3cd159ed4f877de958581bdbb1
hlkirkness <hlkirkness@gmail.com>
2021-11-10 21:16:46 -0500

    began initial function, added more consts

--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@ function initPage(){
     const searchName = document.getElementById("search-name");
     const searchCity = document.getElementById("search-button");
     const clearCity = document.getElementById("clear-history");
-    const cityName = document.getElementById("city-name");
+    const cityNameEl = document.getElementById("city-name");
     const currentTempEl = document.getElementById("temperature");
     const currentHumidityEl = document.getElementById("humidity");
     const currentWindEl = document.getElementById("wind-speed");
     const currentUVEl = document.getElementById("UV-index");
+    const todaysWeather = document.getElementById("todays-weather")
+
+    function todaysForecast(cityName) {
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+        fetch(queryURL)
+            .then(function (response){
+            })
+    }
 }
 
 initPage();
