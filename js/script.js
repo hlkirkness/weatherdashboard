9bc6a762352161e8c762d5d3c9f1a26bc5c71cf2
hlkirkness <hlkirkness@gmail.com>
2021-11-10 20:56:47 -0500

    get element by id const added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-const APIKey = "8726f60f15f35e166930b6a44a0b0406";
+function initPage(){
+    const APIKey = "8726f60f15f35e166930b6a44a0b0406";
+    const searchName = document.getElementById("search-name");
+    const searchCity = document.getElementById("search-button");
+    const clearCity = document.getElementById("clear-history");
+    const cityName = document.getElementById("city-name");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+}
+
+initPage();
