f8cfe9973376417292e2a4e19909704ddd27c293
hlkirkness <hlkirkness@gmail.com>
2021-11-10 21:35:48 -0500

    API calls setup

--- js/script.js
+++ js/script.js
@@ -9,11 +9,19 @@ function initPage(){
     const currentWindEl = document.getElementById("wind-speed");
     const currentUVEl = document.getElementById("UV-index");
     const todaysWeather = document.getElementById("todays-weather")
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
     function todaysForecast(cityName) {
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
         fetch(queryURL)
             .then(function (response){
+                let lat = response.data.coord.lat;
+                let lon = response.data.coord.lon;
+                let UVURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+                fetch(UVURL)
+                let cityID = response.data.id;
+                let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+                fetch(forecastURL)
             })
     }
 }
